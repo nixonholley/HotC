e8ef6b9f03bd4e91b857674f09c6223fd2100df1
Nixon <nixon.holley2@gmail.com>
2024-12-06 18:36:50 -0500

    edited OCR main

--- OCR/Scanner/process_scan.py
+++ OCR/Scanner/process_scan.py
@@ -10,13 +10,8 @@ def main():
     im_file = get_random_image_file()
     im = cv2.imread(im_file)
 
-    edge_detection(im)
-    # apply dilation
-
-    # apply erosion
-
-    #find all countours
-    pass
+    # finds edges of card and creates new image
+    im = edge_detection(im)
 
 if __name__ == '__main__':
     main()
