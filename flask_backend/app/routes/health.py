d1babdf9cad33df5b50010bcd93ec3ef4bfdd42e
Nixon <nixon.holley2@gmail.com>
2024-12-20 15:32:46 -0500

    added current worklist with resources

--- flask_backend/app/routes/health.py
+++ flask_backend/app/routes/health.py
@@ -5,7 +5,7 @@ from routes.utils import parse_database_uri
 
 health_bp = Blueprint('health', __name__)
 
-@health_bp.route('/')
+@health_bp.route('/', methods=['GET'])
 def health_check():
     response = {
         "status": "healthy",
